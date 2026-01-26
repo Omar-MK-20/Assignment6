30f3306165d0f23f3457a18360cb7dc96209ea8b
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2026-01-26 23:41:30 +0200

    implement user signup, update, and retrieval with error handling

--- src/Modules/User/user.controller.js
+++ src/Modules/User/user.controller.js
@@ -1,8 +1,36 @@
 import { Router } from "express";
+import { getByEmail, getById, signup, updateOrCreate } from "./user.service.js";
 
-const userRouter = Router();
+export const userRouter = Router();
 
-userRouter.post("/signup", (req, res) =>
+userRouter.post("/signup", async (req, res) =>
 {
-    
+    const result = await signup(req.body);
+
+    res.status(201).json(result);
+});
+
+userRouter.patch("/:id", async (req, res) =>
+{
+    const { id } = req.params;
+    console.log(req.params);
+    const result = await updateOrCreate(id, req.body);
+
+    res.status(200).json(result);
+});
+
+userRouter.get("/by-email", async (req, res) =>
+{
+    const { email } = req.query;
+    const result = await getByEmail(email);
+
+    res.status(200).json(result);
+});
+
+userRouter.get("/:id", async (req, res) =>
+{
+    const { id } = req.params;
+    const result = await getById(id);
+
+    res.status(200).json(result);
 });
--- src/Modules/User/user.service.js
+++ src/Modules/User/user.service.js
@@ -1,3 +1,71 @@
+import { User } from "../../DB/User/user.model.js";
+import { ResponseError } from "../../util/ResponseError.js";
+
+const passwordValidation = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{5,}$/;
+
 export async function signup(bodyData)
 {
+    const { email, password } = bodyData;
+
+    const existUser = await User.findOne({ where: { email: email } });
+
+    if (existUser)
+    {
+        throw new ResponseError("Email already exist", 409, { email: email });
+    }
+
+    if (!passwordValidation.test(password))
+    {
+        throw new ResponseError("Password must be Minimum 5 characters, at least one uppercase letter, one lowercase letter, one number and one special character", 422, { password: password });
+    }
+
+    try
+    {
+        const newUser = User.build(bodyData);
+        const result = await newUser.save();
+        return { message: "User created successfully", result };
+    }
+    catch (error)
+    {
+        throw new ResponseError("error creating user");
+    }
+
+}
+
+export async function updateOrCreate(userId, bodyData)
+{
+    const { password } = bodyData;
+
+    if (!passwordValidation.test(password))
+    {
+        throw new ResponseError("Password must be Minimum 5 characters, at least one uppercase letter, one lowercase letter, one number and one special character", 422, { password: password });
+    }
+
+    const result = await User.upsert({ ...bodyData, id: userId }, { validate: false });
+
+    return { message: "User Updated successfully", result };
+}
+
+export async function getByEmail(email)
+{
+    const user = await User.findOne({ where: { email: email }, attributes: { exclude: ["password"] } });
+
+    if (!user)
+    {
+        throw new ResponseError("No user found", 404);
+    }
+
+    return { message: "success", user };
+}
+
+export async function getById(userId)
+{
+    const user = await User.findByPk(userId, { attributes: { exclude: ["password", "role"] } });
+
+    if (!user)
+    {
+        throw new ResponseError("no user found");
+    }
+
+    return { message: "success", user };
 }
