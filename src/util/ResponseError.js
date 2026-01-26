62512846819bdd22c69307f16d72fd88853593e6
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2026-01-26 23:42:24 +0200

    refactor ResponseError class to improve parameter naming and add info property

--- src/util/ResponseError.js
+++ src/util/ResponseError.js
@@ -3,11 +3,16 @@ export class ResponseError extends Error
     /**
      * 
      * @param {string} message 
-     * @param {number} status default `500`
+     * @param {number} status Default `500`
+     * @param {object} info Additional information about the cause of the error
      */
-    constructor(message, status = 500)
+    constructor(message, statusCode = 500, info = null)
     {
         super(message);
-        this.status = status;
+        this.name = 'ResponseError';
+        this.statusCode = statusCode;
+        this.info = info;
+
+        Error.captureStackTrace(this, this.constructor);
     }
 }
